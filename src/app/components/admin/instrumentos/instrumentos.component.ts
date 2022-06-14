9004966fb6793236baf1f93c51a517d7e8d20bcc
Mariana Uribe Arbelaez <mariana_uribe82141@elpoli.edu.co>
2022-06-13 20:53:14 -0500

    mensaje al cancelar

--- src/app/components/admin/instrumentos/instrumentos.component.ts
+++ src/app/components/admin/instrumentos/instrumentos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-instrumentos',
@@ -13,6 +14,23 @@ export class InstrumentosComponent implements OnInit {
   modulo: any=this.router.snapshot.paramMap.get('modulo');
   
   ngOnInit(): void {
+  }
+
+  cancelar() {
+    Swal.fire({
+      title: 'Está seguro que desea cancelar?',
+      text: "Será redirigido a {{modulo}}",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#009045',
+      confirmButtonText: 'Confirmar',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.route.navigate(['admin/:modulo']);
+      }
+    })
   }
 
 }
--- src/app/components/admin/salones/salones.component.ts
+++ src/app/components/admin/salones/salones.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-salones',
   templateUrl: './salones.component.html',
@@ -12,6 +13,23 @@ export class SalonesComponent implements OnInit {
   modulo: any=this.router.snapshot.paramMap.get('modulo');
 
   ngOnInit(): void {
+  }
+
+  cancelar() {
+    Swal.fire({
+      title: 'Está seguro que desea cancelar?',
+      text: "Será redirigido a {{modulo}}",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#009045',
+      confirmButtonText: 'Confirmar',
+      cancelButtonColor: '#d33',
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.route.navigate(['admin/:modulo']);
+      }
+    })
   }
 
 }
